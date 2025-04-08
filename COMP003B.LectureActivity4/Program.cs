56470ae62b509afb04b0afb59df868d03f365600
Matthew Tan <matthewtan@my.whccd.edu>
2025-04-07 19:36:48 -0700

    Add project files.

---
+++ COMP003B.LectureActivity4/Program.cs
@@ -0,0 +1,42 @@
+/*
+ * Author: Matthew Tan 
+ * Course: COMP-003B: ASP.NET Core
+ * Faculty: Jonathan Cruz
+ * Purpose: 4th Lecture activity using a shared Razor layout, using partial views and tag helpers.
+ */ 
+namespace COMP003B.LectureActivity4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
